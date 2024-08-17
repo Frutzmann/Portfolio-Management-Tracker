a3136a1c3e33417781221bcdbc44ae3a861f15ce
François UTZMANN <utzmann.francois@gmail.com>
2024-08-17 18:47:12 +0200

    Environment variables editing

--- pft/build_and_dockerize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export SPRING_DATASOURCE_URL=jdbc:postgresql://localhost:5432/pft
-export SPRING_DATASOURCE_USERNAME=postgres
-export SPRING_DATASOURCE_PASSWORD=2105
-
-mvn clean install
